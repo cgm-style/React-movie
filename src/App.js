928c63eedfb287aa586641036596bd4523c9543b
cgm-style <rmsauddhkdwk@naver.com>
2022-10-20 23:20:24 +0900

    깃허브 router 테스트

--- src/App.js
+++ src/App.js
@@ -4,14 +4,10 @@ import Home from "./routes/Home";
 
 function App() {
   return (
-    <Router basename="process.env.PUBLIC_URL">
+    <Router>
       <Routes>
-        <Route path="/movie" element={<Detail />}>
-          <Detail />
-        </Route>
-        <Route path="/" element={<Home />}>
-          <Home />
-        </Route>
+        <Route path="/movie" element={<Detail />}></Route>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
       </Routes>
     </Router>
   );
