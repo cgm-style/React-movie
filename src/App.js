93e82e823684803d2a4a9234883ed96e3d99961e
cgm-style <rmsauddhkdwk@naver.com>
2022-10-21 21:25:39 +0900

    movie React Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/movie" element={<Detail />}></Route>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
+        <Route path={process.env.PUBLIC_URL + `/`} element={<Home />}></Route>
       </Routes>
     </Router>
   );
