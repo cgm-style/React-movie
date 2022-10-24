d3ee5208a7609735679755d69b08c9e140f97441
cgm-style <rmsauddhkdwk@naver.com>
2022-10-24 22:19:06 +0900

    parameter test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/movie" element={<Detail />}></Route>
+        <Route path="/movie/:id" element={<Detail />}></Route>
         <Route path="/" element={<Home />}></Route>
       </Routes>
     </Router>
